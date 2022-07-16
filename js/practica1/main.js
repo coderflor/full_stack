45fa93606ffc320ee7a48f4dc6b0d621d5145e2a
Florencia Fernandez <fl0p4@hotmail.com>
2022-07-16 02:04:56 -0300

    Add changes in login and signup fonts. Add responsiveness to destinations sections and make changes on contact us

---
+++ js/practica1/main.js
@@ -0,0 +1,18 @@
+let nombre = "Florencia";
+let edad = "35";
+
+console.log('Mi nombre es', nombre, 'y tengo', edad, 'años');
+/* alert(salida); */
+
+let apellido = "Fernandez";
+let frecuencia = "comun";
+
+apellido = "Gonzalez";
+frecuencia = "poco comun"
+
+console.log("el apellido", apellido, "es muy", frecuencia)
+
+let primerNumero = 3;
+let segundoNumero = 5;
+
+console.log("la suma de los numeros da: " 'primerNumero' + 'segundoNumero')
